7e7724b45cc899fe876cb7814b488108653ee663
ilkerkaran <ilkerkaran@gmail.com>
2018-07-18 19:28:39 +0300

    Adding Navigation with Event Binding, Passing Recipe Data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() navChange = new EventEmitter<string>();
   constructor() { }
+  onRecipeNavClick() {
+    this.navChange.emit('recipe');
+  }
 
+  onShoppingListNavClick() {
+    this.navChange.emit('shopping');
+  }
   ngOnInit() {
   }
 
