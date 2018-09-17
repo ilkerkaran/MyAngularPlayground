64b7955523fa6c83690cbe45badd4fe66f044fc3
ilkerkaran <ilkerkaran@gmail.com>
2018-09-17 20:03:30 +0300

    end Of Routing Section

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,20 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.editMode = params['new'];
+      this.id = params['id'];
+    });
+  }
+}
